c5337fd7a7b489969d6c5cde2aba4bdfd3502ac0
igor <shkuryn@gmail.com>
2024-02-11 09:25:28 +0200

    add spec for query

--- spec/factories/product_factory.rb
+++ spec/factories/product_factory.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :product do
     sequence(:title) { |n| "Best product ever (#{n})" }
     sequence(:description) { |n| "Best description ever (#{n})" }
-    color { FFaker::Color.name }
+    color { 'green' }
     price { rand(10..100) }
     vendor { 'Test Vendor' }
   end
---
+++ spec/graphql/types/query_type_spec.rb
@@ -0,0 +1,38 @@
+# spec/graphql/types/query_type_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe Types::QueryType do
+  describe '#all_products' do
+    let!(:product1) { create(:product) }
+    let!(:product2) { create(:product) }
+
+    it 'returns all products' do
+      query_string = <<-GRAPHQL
+        query {
+          allProducts {
+            id
+            title
+          }
+        }
+      GRAPHQL
+
+      result = GraphqlTutorialSchema.execute(query_string)
+      all_products = result['data']['allProducts']
+
+      expect(all_products.count).to eq(2)
+
+      product1_attributes = all_products.find { |product| product['id'] == product1.id.to_s }
+      expect(product1_attributes).to include(
+                                       'id' => product1.id.to_s,
+                                       'title' => product1.title,
+                                       )
+
+      product2_attributes = all_products.find { |product| product['id'] == product2.id.to_s }
+      expect(product2_attributes).to include(
+                                       'id' => product2.id.to_s,
+                                       'title' => product2.title,
+                                       )
+    end
+  end
+end
